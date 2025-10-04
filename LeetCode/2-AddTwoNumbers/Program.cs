e6e38806dbd93620ac67c95707ce47b798d3ce84
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-10-04 08:29:11 +0300

    feat: solved LeetCode 2 - Add Two Numbers

---
+++ LeetCode/2-AddTwoNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+using _2_AddTwoNumbers;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+ListNode l1 = new (2, new (4, new (3)));
+ListNode l2 = new (5, new (6, new (4)));
+ListNode l3 = new (9, new (9, new(9, new(9, new(9, new(9, new(9)))))));
+ListNode l4 = new (9, new (9, new(9, new(9))));
+
+AddTwoNumbersSolution solution = new();
+//Console.WriteLine(solution.AddTwoNumbers(l1,l2));
+
+var data = solution.AddTwoNumbers(l3, l4);
+
+while (data != null)
+{
+    Console.Write(data.val + " ");
+    data = data.next;
+}
+
+
+// 2 4 3
+// 5 6 4
+// 

3fc55f2c6667101b4cc5ac3c4c5b71ae887fe4f8
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-10-05 07:10:06 +0300

    feat: solved LeetCode 3 - Longest Substring Without Repeating Characters

---
+++ LeetCode/3-LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using _3_LongestSubstringWithoutRepeatingCharacters;
+
+Solution solution = new Solution();
+string input = "abcabcbb";
+int result = solution.LengthOfLongestSubstring(input);
+Console.WriteLine($"Input: {input}, Length of Longest Substring Without Repeating Characters: {result}");
+
+string input2 = "bbbbb";
+int result2 = solution.LengthOfLongestSubstring(input2);
+Console.WriteLine($"Input: {input2}, Length of Longest Substring Without Repeating Characters: {result2}");
+
+string input3 = "pwwkew";
+int result3 = solution.LengthOfLongestSubstring(input3);
+Console.WriteLine($"Input: {input3}, Length of Longest Substring Without Repeating Characters: {result3}");

f662a9f8ce50ae03becba10d52fb0463578adc76
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-10-05 07:39:13 +0300

    feat: solved LeetCode 4 - Median of Two Sorted Arrays

---
+++ LeetCode/4-MedianOfTwoSortedArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿using _4_MedianOfTwoSortedArrays;
+
+Solution solution = new();
+int[] nums1 = { 1, 3 };
+int[] nums2 = { 2 };
+Console.WriteLine(solution.FindMedianSortedArrays(nums1, nums2));
+
+int[] nums3 = { 1, 2 };
+int[] nums4 = { 3,4 };
+Console.WriteLine(solution.FindMedianSortedArrays(nums3, nums4));

d08908d48e5e29be0c03baf54a9c0e941637d07c
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-10-08 21:13:23 +0300

    feat: solve LeetCode 5 - Longest Palindromic Substring

---
+++ LeetCode/5-LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using _5_LongestPalindromicSubstring;
+
+string s = "a";
+Solution solution = new();
+Console.WriteLine(solution.LongestPalindrome(s));

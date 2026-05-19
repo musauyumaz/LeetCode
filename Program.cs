f748a50ad7355e0c5591d9da921b2359263d44a7
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2026-05-19 16:08:47 +0300

    Add Q00002 AddTwoNumbers solution
    Add a new LeetCode solution for Q00002 (Add Two Numbers) and wire it into Program.cs. Introduces Q00002_AddTwoNumbers.cs which implements AddTwoNumbers using a dummy head and carry handling, includes a ListNode class and a Test() that prints sample results. Program.cs was updated to comment out the Q00001 test and run the Q00002 test instead.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿using LeetCode;
 
 #region Q00001_TwoSum
-Q00001_TwoSum twoSumSolution = new();
-twoSumSolution.Test();
+//Q00001_TwoSum solution = new();
+//solution.Test();
+#endregion
+#region Q00002_AddTwoNumbers
+Q00002_AddTwoNumbers solution = new();
+solution.Test();
 #endregion
---
+++ Q00002_AddTwoNumbers.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode
+{
+    public sealed class Q00002_AddTwoNumbers
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode dummy = new ListNode(0);
+            ListNode current = dummy;
+            int carry = 0;
+
+            while (l1 != null || l2 != null)
+            {
+                int x = (l1 != null) ? l1.val : 0;
+                int y = (l2 != null) ? l2.val : 0;
+
+                int sum = x + y + carry;
+                carry = sum / 10;
+
+                current.next = new ListNode(sum % 10);
+                current = current.next;
+
+                if (l1 != null) l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
+            }
+
+
+            if (carry > 0)
+            {
+                current.next = new ListNode(carry);
+            }
+
+            return dummy.next;
+        }
+
+        public void Test()
+        {
+            ListNode l1 = new(2, new(4, new(3)));
+            ListNode l2 = new(5, new(6, new(4)));
+            ListNode l3 = new(9, new(9, new(9, new(9, new(9, new(9, new(9)))))));
+            ListNode l4 = new(9, new(9, new(9, new(9))));
+
+            Q00002_AddTwoNumbers solution = new();
+            //Console.WriteLine(solution.AddTwoNumbers(l1,l2));
+
+            var data = solution.AddTwoNumbers(l3, l4);
+
+            while (data != null)
+            {
+                Console.Write(data.val + " ");
+                data = data.next;
+            }
+        }
+    }
+    public sealed class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}

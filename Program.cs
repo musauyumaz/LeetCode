28854b44ce289601477af4682f8e43e38841d4f0
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2026-05-19 16:04:34 +0300

    feat(algorithm): add TwoSum solution with test cases
    - Added `Q00001_TwoSum` class in `Q00001_TwoSum.cs` to implement the TwoSum algorithm and its test cases for solving LeetCode problem #1.
    - Updated `Program.cs` to include and execute the `Q00001_TwoSum.Test()` method for validating the solution with sample inputs.
    - No breaking changes; this is a new feature addition.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using LeetCode;
+
+#region Q00001_TwoSum
+Q00001_TwoSum twoSumSolution = new();
+twoSumSolution.Test();
+#endregion
---
+++ Q00001_TwoSum.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode
+{
+    public sealed class Q00001_TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dictionary = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (dictionary.ContainsKey(diff))
+                    return new[] { dictionary[diff], i };
+                dictionary[nums[i]] = i;
+            }
+
+            return null;
+        }
+
+        public void Test()
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int[] nums2 = { 3, 2, 4 };
+            int[] nums3 = { 3, 3 };
+            Q00001_TwoSum twoSumSolution = new();
+
+            Array.ForEach(twoSumSolution.TwoSum(nums, 9), x => Console.Write(x + ", "));
+            Console.WriteLine();
+            Array.ForEach(twoSumSolution.TwoSum(nums2, 6), x => Console.Write(x + ", "));
+            Console.WriteLine();
+            Array.ForEach(twoSumSolution.TwoSum(nums3, 6), x => Console.Write(x + ", "));
+        }
+    }
+}

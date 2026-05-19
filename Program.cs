c7ce7de2bd2be057df8a45e4f83d2175fb850c17
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2026-05-19 16:11:51 +0300

    Add Q00003 LengthOfLongestSubstring
    Add new Q00003_LengthOfLongestSubstring solution implementing LengthOfLongestSubstring using a moving substring and IndexOf to drop duplicates. Update Program.cs to enable the Q00003 test (Q00002 test commented out). Includes a Test() method that runs example inputs "abcabcbb", "bbbbb", and "pwwkew" and prints results.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
 //solution.Test();
 #endregion
 #region Q00002_AddTwoNumbers
-Q00002_AddTwoNumbers solution = new();
+//Q00002_AddTwoNumbers solution = new();
+//solution.Test();
+#endregion
+#region Q00003_LengthOfLongestSubstring
+Q00003_LengthOfLongestSubstring solution = new();
 solution.Test();
 #endregion
---
+++ Q00003_LengthOfLongestSubstring.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode
+{
+    public sealed class Q00003_LengthOfLongestSubstring
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            string currentSubstring = string.Empty;
+            int maxLength = 0;
+            foreach (char c in s)
+            {
+                int index = currentSubstring.IndexOf(c);
+                if (index != -1)
+                {
+                    currentSubstring = currentSubstring[(index + 1)..];
+                }
+                currentSubstring += c;
+                maxLength = Math.Max(maxLength, currentSubstring.Length);
+            }
+            return maxLength;
+        }
+
+        public void Test()
+        {
+            Q00003_LengthOfLongestSubstring solution = new();
+            string input = "abcabcbb";
+            int result = solution.LengthOfLongestSubstring(input);
+            Console.WriteLine($"Input: {input}, Length of Longest Substring Without Repeating Characters: {result}");
+
+            string input2 = "bbbbb";
+            int result2 = solution.LengthOfLongestSubstring(input2);
+            Console.WriteLine($"Input: {input2}, Length of Longest Substring Without Repeating Characters: {result2}");
+
+            string input3 = "pwwkew";
+            int result3 = solution.LengthOfLongestSubstring(input3);
+            Console.WriteLine($"Input: {input3}, Length of Longest Substring Without Repeating Characters: {result3}");
+        }
+    }
+}
